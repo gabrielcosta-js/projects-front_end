b87f6b6cda58382fd6dee21a825f501ee4ec0a81
Gabriel Costa JS <106637877+gabrielcosta-js@users.noreply.github.com>
2026-04-20 19:56:43 -0300

    correcoes no layout e reponsividade, animacao nas sections

--- project-center/Src/scripts/main.js
+++ project-center/Src/scripts/main.js
@@ -3,6 +3,7 @@ import * as script from './script.js';
 // Carregar o site
 script.paginaAtiva();
 script.scrollAtivo();
+script.animarScroll();
 script.tema();
 script.coresProjetosFerramentas();
 script.expandirCertificado();
